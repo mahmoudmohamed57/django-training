aa5733de63688fa4c57b32eb17c1a831f4ffdf89
mahmoudmohamed57 <mahmoudmohamed572000@gmail.com>
2022-10-14 18:08:30 +0200

    feat & docs

--- artists/views.py
+++ artists/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from artists.models import Artist
+from .forms import ArtistForm
 
 # Create your views here.
+
+
+def create(request):
+    if request.method == 'POST':
+        form = ArtistForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = ArtistForm()
+    return render(request, 'createArtist.html', {'form': form})
+
+
+def list(request):
+    return render(request, 'listArtist.html', {'query_set': Artist.objects.all().prefetch_related('album_set')})
